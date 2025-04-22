3b56ad4e56a3cdd92a56e36440669aede211bfcf
Bhavana Pulla <150841718+Bhavanapulla@users.noreply.github.com>
2025-04-22 18:49:02 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        String name = "Bhavana";
+      //concatinating 2 strings
+        name = name.concat(" Pulla");
+        System.out.println(name);
+      //output shows name concatinated
+    }
+}
